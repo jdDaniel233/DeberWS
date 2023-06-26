ccfd9851a9148277ed3f1d178deba22ab77df27a
Daniel <simbanadaniel42@gmail.com>
2023-06-26 16:55:30 -0500

    agregar programacion a los botones

--- app/src/main/java/com/example/deberws/FgeneralActivity.java
+++ app/src/main/java/com/example/deberws/FgeneralActivity.java
@@ -2,7 +2,9 @@ package com.example.deberws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FgeneralActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class FgeneralActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fgeneral);
+    }
+    public void irAtras(View view){
+        Intent intent=new Intent(this,MainActivity.class);
+        view.getContext().startActivity(intent);
     }
 }
